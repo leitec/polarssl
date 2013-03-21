f779563592ce7b342e91e6f0b611301f809ed14e
Claudio Leite <leitec@staticky.com>
2013-03-21 19:18:19 -0400

    Adding initial Mac changes

---
+++ include/polarssl/net_mactcp.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#include <Types.h>
+#include <MacTCP.h>
+#include <AddressXlation.h>
+
+#include "polarssl/config.h"
+
+typedef struct MacTCP_Instance {
+	short refnum;
+	short initialized;
+	ResultUPP mactcp_lookupdone_upp;
+	TCPNotifyUPP mactcp_asr_upp;
+} mactcp_inst;
+
+typedef struct MacTCP_Conn {
+	mactcp_inst *i;
+	StreamPtr s;
+	int readable;
+} mactcp_conn;
+
+static pascal void mactcp_lookupdone(struct hostInfo * hi, char *cookie);
+static pascal void mactcp_asr(StreamPtr str, unsigned short event, Ptr cookie,unsigned short termin_reason, struct ICMPReport * icmp);
+void mactcp_close(mactcp_conn *c);
+int mactcp_send(void *vc, const unsigned char *buf, size_t len);
+int mactcp_recv(void *vc, unsigned char *inbuf, size_t len);
+
+/*OSErr mactcp_send(mactcp_conn *c, char *buf, size_t len);
+OSErr mactcp_recv(mactcp_conn *c, char *inbuf, size_t len);*/
+OSErr mactcp_connect(mactcp_inst *i, mactcp_conn *c, char *hostname, short port);
+OSErr mactcp_init(mactcp_inst *i);
---
+++ library/net_mactcp.c
@@ -0,0 +1,207 @@
+#include "polarssl/net_mactcp.h"
+#include "polarssl/net.h"
+
+ProcessSerialNumber psn;
+
+static pascal void
+mactcp_lookupdone(struct hostInfo * hi, char *cookie)
+{
+	volatile int   *donep = (int *) cookie;
+
+	*donep = TRUE;
+}
+
+static pascal void
+mactcp_asr(StreamPtr str, unsigned short event, Ptr cookie,
+	   unsigned short termin_reason, struct ICMPReport * icmp)
+{
+	WakeUpProcess(&psn);
+}
+
+void
+mactcp_close(mactcp_conn *c)
+{
+	TCPiopb         pb;
+	OSErr	err;
+
+	pb.ioCRefNum = c->i->refnum;
+	pb.csCode = TCPClose;
+	pb.tcpStream = c->s;
+	pb.csParam.close.validityFlags = 0;
+	pb.csParam.close.userDataPtr = (Ptr) c;
+	PBControlSync((ParmBlkPtr) & pb);
+
+	pb.ioCRefNum = c->i->refnum;
+	pb.csCode = TCPAbort;
+	pb.tcpStream = c->s;
+	pb.csParam.abort.userDataPtr = (Ptr) c;
+	PBControlSync((ParmBlkPtr) & pb);
+
+	pb.ioCRefNum = c->i->refnum;
+	pb.csCode = TCPRelease;
+	pb.tcpStream = c->s;
+	pb.csParam.create.userDataPtr = (Ptr) c;
+	err = PBControlSync((ParmBlkPtr) & pb);
+	
+	if (err == noErr)
+		free(pb.csParam.create.rcvBuff);
+}
+
+int mactcp_send(void *vc, const unsigned char *buf, size_t len)
+{
+	OSErr err;
+	TCPiopb pb;
+	wdsEntry wds[2];
+	mactcp_conn *c = vc;
+	
+	wds[0].length = len;
+	wds[0].ptr = (char *)buf;
+	wds[1].length = 0;
+	
+	pb.ioCRefNum = c->i->refnum;
+	pb.csCode = TCPSend;
+	pb.tcpStream = c->s;
+	pb.csParam.send.validityFlags = 0;
+	pb.csParam.send.pushFlag = TRUE;
+	pb.csParam.send.urgentFlag = 0;
+	pb.csParam.send.wdsPtr = (Ptr)wds;
+	
+	err = PBControlSync((ParmBlkPtr) &pb);
+	
+	if(err == noErr) {
+		return len;
+	} else {
+		printf("mactcp_send(): %d\n", err);
+		return(POLARSSL_ERR_NET_SEND_FAILED);
+	}
+	
+	return err;
+}
+
+int mactcp_recv(void *vc, unsigned char *inbuf, size_t len)
+{
+	TCPiopb pb;
+	OSErr err;
+	mactcp_conn *c = vc;
+	
+	pb.ioCRefNum = c->i->refnum;
+	pb.csCode = TCPRcv;
+	pb.tcpStream = c->s;
+	pb.csParam.receive.commandTimeoutValue = 2;
+	pb.csParam.receive.rcvBuff = (char *)inbuf;
+	pb.csParam.receive.rcvBuffLen = len;
+	
+	err = PBControlSync((ParmBlkPtr) &pb);
+	
+	if(err == noErr) {
+		return pb.csParam.receive.rcvBuffLen;
+	} else {
+		if(err == connectionClosing)
+			return 0;
+		
+		printf("mactcp_recv(): %d\n", err);
+		return(POLARSSL_ERR_NET_RECV_FAILED);
+	}
+}
+
+OSErr mactcp_connect(mactcp_inst *i, mactcp_conn *c, char *hostname, short port)
+{
+	volatile int resolverDone = FALSE;
+	struct hostInfo hi;
+	size_t buflen;
+	UDPiopb upb;
+	TCPiopb pb;
+	OSErr err;
+	
+	if(!i->initialized) {
+		printf("mactcp_connect(): not initialized\n");
+		return -1;
+	}
+	
+	c->i = i;
+	err = StrToAddr(hostname, &hi, i->mactcp_lookupdone_upp, (char *)&resolverDone);
+	if(err == cacheFault) {
+		while(!resolverDone)
+			continue;
+	}
+
+	if(err != noErr && err != cacheFault)
+		return err;
+		
+	/* get the max MTU size for this path */
+	/* we use this to calculate the buffer size */
+	upb.ioCRefNum = i->refnum;
+	upb.csCode = UDPMaxMTUSize;
+	upb.csParam.mtu.remoteHost = hi.addr[0];
+	err = PBControlSync((ParmBlkPtr) &upb);
+	
+	if(err != noErr) {
+		printf("mactcp_connect(): error getting MTU, err=%d\n", err);
+		return err;
+	}
+	
+	buflen = upb.csParam.mtu.mtuSize * 4 + 1024;
+	if(buflen < 4096)
+		buflen = 4096;
+		
+	pb.ioCRefNum = i->refnum;
+	pb.csCode = TCPCreate;
+	pb.csParam.create.rcvBuff = malloc(buflen);
+	pb.csParam.create.rcvBuffLen = buflen;
+	pb.csParam.create.notifyProc = i->mactcp_asr_upp;
+	pb.csParam.create.userDataPtr = (Ptr)c;
+	
+	err = PBControlSync((ParmBlkPtr) &pb);
+	if(err != noErr) {
+		printf("mactcp_connect(): error creating TCP stream, err=%d", err);
+		return err;
+	}
+	
+	c->s = pb.tcpStream;
+	
+	pb.ioCRefNum = i->refnum;
+	pb.csCode = TCPActiveOpen;
+	pb.tcpStream = c->s;
+	pb.csParam.open.validityFlags = 0;
+	pb.csParam.open.remoteHost = hi.addr[0];
+	pb.csParam.open.remotePort = port;
+	pb.csParam.open.localPort = 0;
+	pb.csParam.open.timeToLive = 0;
+	pb.csParam.open.security = 0;
+	pb.csParam.open.optionCnt = 0;
+	pb.csParam.open.userDataPtr = (Ptr)c;
+	err = PBControlSync((ParmBlkPtr) &pb);
+	if(err != noErr) {
+		printf("Error connecting TCP stream: %d\n", err);
+		return err;
+	}
+	
+	c->readable = 1;
+	
+	return noErr;
+}
+
+OSErr mactcp_init(mactcp_inst *i)
+{
+	OSErr err;
+	
+	err = OpenDriver("\p.IPP", &(i->refnum));
+	if(err != noErr) {
+		printf("Error initializing driver\n");
+		return err;
+	}
+		
+	err = OpenResolver(NULL);
+	if(err != noErr) {
+		printf("Error initializing resolver\n");
+		return err;
+	}
+	
+	i->mactcp_lookupdone_upp = NewResultProc(&mactcp_lookupdone);
+	i->mactcp_asr_upp = NewTCPNotifyProc(&mactcp_asr);
+	i->initialized = 1;
+	
+	GetCurrentProcess(&psn);
+
+	return noErr;
+}
